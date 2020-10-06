96c867e99dfa9fad32cc536d276057745079449a
sooraj <sbkaiprath99@gmail.com>
2020-10-06 11:28:08 +0530

    update and delete event method

--- backend/controller/event.js
+++ backend/controller/event.js
@@ -1,13 +1,13 @@
 
 const asyncHandler = require('../middlewares/async');
+const ErrorResponce = require('../utils/errorResponce');
 //GET all team members datas
 // @access Public
 //METHOD GET
 exports.getAllEvents = asyncHandler(async (req, res, next) => {
 
-
     res.status(200).render('./pages/events', { event: res.advancedQueryResults });
-   
+
 
 });
 
@@ -27,4 +27,32 @@ exports.createEvent = asyncHandler(async (req, res, next) => {
         data: event,
 
     })
+});
+
+//Update a team member data
+// @access Private
+//METHOD PUT
+exports.updateEvent = asyncHandler(async (req, res, next) => {
+    const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!event) {
+        return next(new ErrorResponce('Cannot update team member data', 404))
+    }
+
+    res.status(200).json({ success: true, data: event })
+});
+
+
+//Delete team member data
+// @access Private
+//METHOD DELETE
+
+exports.deleteEvent = asyncHandler(async (req, res, next) => {
+    const event = await Event.findById(req.params.id);
+    if (!event) {
+        return next(new ErrorResponce('Not successful in deleting the team member data', 404))
+    }
+
+    event.remove()
+
+    res.status(200).json({ success: true, data: event })
 })
